dc8815849c9619a9f4d71340fb61ae288df9b3c2
root <you@example.com>
2017-03-23 14:30:05 +0100

    add changes

--- index.php
+++ index.php
@@ -1,22 +1,31 @@
 <html>
+	<head>
 
+	<meta charset="UTF-8">
+	</head>
 
 	<body>
 
 	<h1>Hello les Wilders</h1>
-<?php
+	<?php
 
-	
-try {
-    $dbh = new PDO('mysql:host=localhost;dbname=test', $user, $pass);
-    foreach($dbh->query('SELECT * from actors') as $row) {
-        print_r($row);
-    }
-    $dbh = null;
-} catch (PDOException $e) {
-    print "Erreur !: " . $e->getMessage() . "<br/>";
-    die();
-}
+	$user = 'root';
+	$pass = 'zuzu';
 
-?>
+	try {
+	    $dbh = new PDO('mysql:host=localhost;dbname=cineacademy', $user, $pass);
+	    foreach($dbh->query('SELECT * from actors') as $row) {
+		echo '<li>'.$row['firstname']. ' ' . $row['lastname']. '</li>';
+	    }
+	    $dbh = null;
+	} catch (PDOException $e) {
+	    print "Erreur !: " . $e->getMessage() . "<br/>";
+	    die();
+	}
 
+	?>
+
+
+</body>
+
+</html>
